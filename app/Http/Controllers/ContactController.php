6ad041a6fe2add8f81dfc38a4bc3d450f1ab6823
jorbinogales <jorbinogales@gmail.com>
2021-07-02 11:13:28 -0400

    'date'

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use Exception;
+use Mail;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ContactRequest $request)
+    {
+        try {
+
+            // redes.dacapri@gmail.com
+            $date = $request->all();
+
+            Contact::create($request->validated());
+
+            // Mail::to('jorbinogales@gmail.com')->send(new ContactMail($date));
+
+            return 'success';
+
+        } catch (Exception $e){
+            return 'error';
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        //
+    }
+}
